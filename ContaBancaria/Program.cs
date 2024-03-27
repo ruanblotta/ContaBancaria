9fc04cdbeb9d48bf0ca17631a4eaee65adc0d2e4
Ruan Blotta <ruanblotta@gmail.com>
2024-03-27 15:15:51 -0300

    First Commit: Implementando Chave de Acesso para acesso à Conta Bancária

---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string nConta;
+
+            Console.WriteLine("Digite o número da sua Conta Bancária: ");
+            nConta = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu Nome: ");
+            nome = Console.ReadLine();
+
+            if (nome == "Ruan" && nConta == "12345")
+            {
+                Console.WriteLine("Acesso Aprovado. Seja bem vindo " + nome);
+            } else
+            {
+                Console.WriteLine("Acesso Negado. Tente Novamente.");
+            }
+
+
+
+        }
+    }
+}
