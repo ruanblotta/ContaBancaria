b49a13f1228c7a751ab3c30d7b4addd4384d924f
Ruan Blotta <ruanblotta@gmail.com>
2024-03-28 10:56:52 -0300

    Refatoração e Implementação de Orientação a Objetos. Modificado algumas variáveis para melhor compreensão do programa

---
+++ ContaBancaria/ContaCliente.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    public class ContaCliente
+    {
+        public string NumeroConta;
+        public string NomeCliente;
+        public double Saldo = 0;
+        public double SaldoAtual;
+ 
+
+        // Construtor para o cliente acessar a conta
+
+        public ContaCliente(string numeroConta, string nomeCliente)
+        {
+            NumeroConta = numeroConta;
+            NomeCliente = nomeCliente;
+        }
+
+        // Método para acessar a conta
+        public void AcessoContaBancaria()
+        {
+            
+            if (NomeCliente == "Ruan" && NumeroConta == "12345")
+            {
+                Console.WriteLine("Acesso Aprovado. Seja bem vindo " + NomeCliente);
+            }
+            else
+            {
+                Console.WriteLine("Acesso Negado. Tente Novamente.");
+            }
+        }
+
+        // Método de Depósito 
+
+        public void Deposito()
+        {
+            Console.WriteLine("Digite o valor à ser depositado");
+            double deposito = double.Parse(Console.ReadLine());
+
+            Saldo += deposito;
+            Console.WriteLine($"O depósito de R$ {deposito.ToString("F2")} foi feito com sucesso!");
+        }
+
+        // Método de Saque e a Cobrança de Taxa de Saque
+        public void Saque()
+        {
+            Console.WriteLine("Digite o valor de saque. ATENÇÃO: É cobrado um valor de R$ 5,00 por cada saque realizado.");
+            double saque = double.Parse(Console.ReadLine());
+            double taxaSaque = 5.00;
+
+            Saldo -= (saque + taxaSaque);
+            Console.WriteLine("Seu saldo atual é de: R$" + Saldo.ToString("F2"));
+        }
+
+        // método verificador de saldo
+
+    /*    public void SaldoAtualConta()
+        {
+            Console.WriteLine("Seu saldo atual é de: R$ " + SaldoAtual.ToString("F2"));
+        }*/
+
+        // Método verificador de saldo inicial
+        public void SaldoInicial()
+        {
+            double Saldo = 0.0;
+            Console.WriteLine($"Seu saldo atual é de: R$ {Saldo.ToString("F2")} ");
+        }
+
+    }
+}
--- ContaBancaria/Program.cs
+++ ContaBancaria/Program.cs
@@ -6,48 +6,22 @@ namespace ContaBancaria
     {
         static void Main(string[] args)
         {
-            string nome;
-            string nConta;
-            double saldo = 0;
+            ContaCliente contaCliente = new ContaCliente("12345", "Ruan"); 
            
-            // Chaves de Acesso
-
             Console.WriteLine("Digite o número da sua Conta Bancária: ");
-            nConta = Console.ReadLine();
+            contaCliente.NumeroConta = Console.ReadLine();
 
             Console.WriteLine("Digite o seu Nome: ");
-            nome = Console.ReadLine();
+            contaCliente.NomeCliente = Console.ReadLine();
 
-            if (nome == "Ruan" && nConta == "12345")
-            {
-                Console.WriteLine("Acesso Aprovado. Seja bem vindo " + nome);
-            } else
-            {
-                Console.WriteLine("Acesso Negado. Tente Novamente.");
-            }
+            contaCliente.AcessoContaBancaria();
 
-            // Funcionalidade de Depósito 
+            contaCliente.SaldoInicial();
+            contaCliente.Deposito();
+            contaCliente.Saque();
+            /*contaCliente.SaldoAtualConta();*/
 
-            Console.WriteLine($"Seu saldo atual é de: R$ {saldo.ToString("F2")} ");
-
-            Console.WriteLine("Digite o valor à ser depositado");
-            double deposito = double.Parse(Console.ReadLine());
-
-            double saldoAtual = saldo += deposito;
-            Console.WriteLine($"O depósito de R$ {deposito.ToString("F2")} foi feito com sucesso!");
-
-
-            Console.WriteLine("Seu saldo atual é de: R$ " + saldoAtual.ToString("F2"));
-
-            // Funcionalidade de Saque e a Cobrança de Taxa de Saque
-
-            Console.WriteLine("Digite o valor de saque. ATENÇÃO: É cobrado um valor de R$ 5,00 por cada saque realizado.");
-            double saque = double.Parse(Console.ReadLine());
-            double taxaSaque = 5.00;
-            double saldoAtualizado = (saldoAtual - taxaSaque) - saque;
-
-            Console.WriteLine("Seu saldo atual é de: R$" + saldoAtualizado.ToString("F2"));
-           
+            Console.WriteLine("Obrigado por utilizar o nosso banco!");
         }
     }
 }
